8e0e59c4b675bf638e4b7d6cb8999d914540064c
Mohammed Al-Mahdawi <mohammed@mostasharoon.org>
2016-05-05 10:41:31 +0300

    First commit

---
+++ classes/frontend/FVB_Core.php
@@ -0,0 +1,167 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+class FVB_Core {
+	function __construct() {
+		add_filter( 'the_content', array( $this, 'fvb_the_content_filter' ), 20 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
+	}
+
+	function fvb_the_content_filter( $content ) {
+
+		if ( ! is_single() ) {
+			return $content;
+		}
+
+		global $post;
+		$form_id = get_post_meta( $post->ID, '_fms_form_id', true );
+
+		if ( empty ( $form_id ) ) {
+			return $content;
+		}
+
+		//get the post from the meta key
+		$args = array(
+			'post_type'  => 'fvb_views',
+			'meta_query' => array(
+				array(
+					'key'     => 'fvb_form',
+					'value'   => (int) $form_id,
+					'compare' => '='
+				)
+			)
+		);
+
+		$posts = get_posts( $args );
+		if ( ! ( empty( $posts ) ) ) {
+			$view_id = $posts[0]->ID;
+		} else {
+			return $content;
+		}
+
+		$view_settings = get_post_meta( $view_id, 'fvb_settings', true );
+
+		if ( $view_settings['restrict'] == 'yes' && ! FVB_CanAccess( $view_settings ) ) {
+			$wrapper_class = empty( $view_settings['wrapper_class'] ) ? '' : $view_settings['wrapper_class'] . ' ';
+
+			return $content . '<br />' . '<div class="' . $wrapper_class . 'isa_error"><span>' . $view_settings['restriction_message'] . '</span></div>';
+		}
+
+		$fields_html = $this->get_custom_fields( $form_id, $view_settings );
+
+		return $content . $fields_html;
+	}
+
+	function scripts() {
+		if ( is_admin() ) {
+			return;
+		}
+
+		if ( fvb_get_option( 'disable_styles', 'fvb_general_settings', '' ) != 'yes' ) {
+			wp_enqueue_style( 'fvb-frontend-css', FVB_URL . 'css/frontend/fvb.css', array( 'fms-colorbox-css' ) );
+		}
+	}
+
+	function get_custom_fields( $form_id, $view_settings ) {
+
+		$wrapper_class = empty( $view_settings['wrapper_class'] ) ? '' : $view_settings['wrapper_class'] . ' ';
+		$html          = '<div class="' . $wrapper_class . 'fvb">';
+		if ( ! empty( $view_settings['title'] ) ) {
+			$html .= '<div class="fvb-header">';
+			$html .= '<h3>' . $view_settings['title'] . '</h3>';
+			$html .= '</div>';
+		}
+
+		global $fvb_from;
+		$fvb_from = '';
+
+		if ( is_array( $view_settings['fields'] ) ) {
+			foreach ( $view_settings['fields'] as $field ) {
+				$html .= $this->render_field( $field, $view_settings );
+			}
+		}
+
+		$html .= '</div>';
+		$html = apply_filters( 'fvb_fields_html', $html, $form_id, $view_settings );
+
+		return $html;
+	}
+
+	function render_field( $field, $view_settings ) {
+
+		$field = apply_filters( 'fvb_field', $field, $view_settings );
+
+		global $post;
+		$html = '';
+
+
+		$text_fields   = array(
+			'text_field',
+			'textarea_field',
+			'email_address',
+			'website_url',
+			'custom_hidden_field',
+			'date_field',
+			'date_time_field',
+			'stepper'
+		);
+		$media_fields  = array( 'image_upload', 'file_upload' );
+		$custom_fields = array( 'radio_field', 'dropdown_field', 'multiple_select', 'checkbox_field', 'repeat_field' );
+
+
+		$value = get_post_meta( $post->ID, $field['name'], true );
+
+		if ( in_array( $field['template'], $text_fields ) || ( in_array( $field['template'], $custom_fields ) ) ) {
+
+
+			$html .= ! empty( $field['new_label'] ) ? '<p><strong>' . $field['new_label'] . ': </strong>' : '<p>';
+
+			if ( is_array( $value ) ) {
+				$html .= apply_filters( 'fvb_field_value', implode( ', ', $value ), $field, $view_settings );
+			} else {
+				$html .= apply_filters( 'fvb_field_value', $value, $field, $view_settings );
+			}
+			$html .= '</p>';
+
+		} elseif ( in_array( $field['template'], $media_fields ) ) {
+
+			$html .= ! empty( $field['new_label'] ) ? '<p><strong>' . $field['new_label'] . ': </strong><br />' : '<p>';
+			if ( is_array( $value ) ) {
+
+				if ( $field['template'] == 'image_upload' ) {
+					$html .= '<div class="fms-container">';
+				}
+
+				foreach ( $value as $single_val ) {
+					$attachment_id = (int) $single_val;
+					$url           = esc_url( wp_get_attachment_url( $attachment_id ) );
+
+					if ( $field['template'] == 'image_upload' ) {
+						$field_html = '<div class="fms-item"> <a class="fms-gallery" href="' . $url . '"><img src="' . $url . '" height="140" width="140"></a></div>';
+						$field_html = apply_filters( 'fvb_field_value', $field_html, $field, $view_settings );
+						$html .= $field_html;
+					} else {
+						$field_html = '<a href="' . $url . '">' . basename( $url ) . '</a><br />';
+						$field_html = apply_filters( 'fvb_field_value', $field_html, $field, $view_settings );
+						$html .= $field_html;
+					}
+
+				}
+
+				if ( $field['template'] == 'image_upload' ) {
+					$html .= '</div>';
+				}
+
+			}
+			$html .= '</p>';
+		}
+
+		$html = apply_filters( 'fvb_field_html', $html, $field, $value, $view_settings );
+
+		return $html;
+	}
+}
+
+$FVB_Core = new FVB_Core();
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Forms Management System Views Builder ===
+Contributors: Engr.MTH, MOSTASHAROON
+Donate link: http://mostasharoon.org/buy-us-a-coffee/
+Tags: forms management system, visual builder, custom fields, custom post type, form, forms, mostasharoon,views
+Requires at least: 3.5
+Tested up to: 4.2.4
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+> This Plugin requires [Forms Management System](http://mostasharoon.org/wordpress/plugins/forms-management-system/).
+> We don't give any support to this plugin here, if you need any help please feel free to contact us through our [support page](http://mostasharoon.org/support/).
+
+Forms Management System Views Builder is a free add-on allows you to display the custom fields at the frontend with easiest way possible.
+
+Features of the plugin include:
+
+* Unlimited views, you can create as many views as you want, but each form should have only one view.
+* Displaying the custom fields at the frontend and control which field will be displayed and change the field order and put a new label for the field.
+* Restrict the custom fields section access with a message for: logged in users only, or choose the role(s) that you want to allow them to access the custom fields section.
+* Add a wrapper class to styles the custom fields as you want.
+* You can disable the default style.
+* You can put a label for the custom fields section.
+
+== Installation ==
+
+1. Upload the entire `forms-management-system-views-builder` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to FMS => Views => to create a new view
+4. Go to FMS => Views Settings and configure the plugin
+
+== Frequently Asked Questions ==
+
+= Where Can I Get Support For This Plugin? =
+
+We don't give any support to this plugin here, if you need any help please feel free to contact us through our [support page](http://mostasharoon.org/support/).
+
+== Changelog ==
+
+= 1.1 =
+* New: Added new hooks.
+
+= 1.0 =
+* initial release
